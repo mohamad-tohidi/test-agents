1cce11dfcecb2729b29c38ff411bbf6af18f80dc
mohamad-tohidi <the.mohammad.tohidi@gmail.com>
2026-04-15 12:59:25 +0330

    first failed test

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import os
+from dotenv import load_dotenv
+from smolagents import CodeAgent, DuckDuckGoSearchTool, InferenceClientModel
+
+load_dotenv()
+
+model = InferenceClientModel(
+    model_id="google/gemma-3-4b-it",
+    token=os.getenv("HF_TOKEN"),
+)
+
+agent = CodeAgent(
+    tools=[DuckDuckGoSearchTool()],
+    model=model,
+)
+
+
 def main():
     print("Hello from test-agents!")
+    result = agent.run(
+        "How many seconds would it take for a leopard at full speed to run through Pont des Arts?"
+    )
+    print(result)
 
 
 if __name__ == "__main__":
